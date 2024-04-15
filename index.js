1b8f70e60e4f722f00129fa019edbdc4f0c0ac49
Yogesh Bhatt <yogeshbhatt9761@gmail.com>
2024-04-15 22:11:00 +0530

    be for link-shortner

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+
+const connectToMongoDB = require("./connect.js");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url.js");
+
+const app = express();
+const PORT = 300;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
+  console.log("MongoDB connected");
+});
+
+
+
+
+
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  try {
+    console.log("entered");
+    const shortId = req.params.shortId;
+    console.log(shortId);
+    const entry = await URL.findOneAndUpdate(
+      {
+        shortId,
+      },
+      {
+        $push: {
+          visitHistory: { timeStamp: Date.now() },
+        },
+      }
+    );
+    res.redirect(entry.redirectURL);
+  } catch (err) {
+    console.log("err0r");
+  }
+});
+
+app.listen(PORT, () => console.log(`server started at PORT - ${PORT}`));
